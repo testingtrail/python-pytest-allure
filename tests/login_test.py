eba6a838dfb4325f2f8f9997f9127e03be7c37b4
jorge.quiros.torres <jorge.quiros.torres@outlook.com>
2019-01-09 15:17:13 -0600

    Pytest implemented and working properly for login_test.py file

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,23 +1,29 @@
 from selenium import webdriver
+import pytest
 
-driver = webdriver.Chrome(executable_path="../drivers/chromedriver.exe")
+class TestLogin():
 
-driver.implicitly_wait(5)
-driver.maximize_window()
+    @pytest.fixture(scope="class")
+    def test_setup(self):
+        global driver
+        driver = webdriver.Chrome(executable_path="../drivers/chromedriver.exe")
+        driver.implicitly_wait(5)
+        driver.maximize_window()
+        yield
+        driver.close()
+        driver.quit()
+        print("Test completed.")
 
-driver.get("https://opensource-demo.orangehrmlive.com/")
+    def test_login(self, test_setup):
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        driver.find_element_by_id("txtUsername").send_keys("Admin")
+        driver.find_element_by_id("txtPassword").send_keys("admin123")
+        driver.find_element_by_id("btnLogin").click()
 
-driver.find_element_by_id("txtUsername").send_keys("Admin")
-driver.find_element_by_id("txtPassword").send_keys("admin123")
-driver.find_element_by_id("btnLogin").click()
+    def test_logout(self, test_setup):
+        driver.find_element_by_id("welcome").click()
+        driver.find_element_by_link_text("Logout").click()
 
-driver.find_element_by_id("welcome").click()
-driver.find_element_by_link_text("Logout").click()
-
-driver.close()
-driver.quit()
-
-print("Test completed.")
 
 
 
